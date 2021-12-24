4763ba9e4e745f25e0c8cd60c220f2d79610c04f
Sascha-Zverev <86235995+Sascha-Zverev@users.noreply.github.com>
2021-12-24 16:25:54 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDk826_jzfzwgi5bIyh51s1XX4-ToTQKHM",
+    authDomain: "superchat-aa955.firebaseapp.com",
+    databaseURL: "https://superchat-aa955-default-rtdb.firebaseio.com",
+    projectId: "superchat-aa955",
+    storageBucket: "superchat-aa955.appspot.com",
+    messagingSenderId: "1092208139837",
+    appId: "1:1092208139837:web:a2de18ec7c9cce4198d454"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
